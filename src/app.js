1dc22e9c7d23b8fd007be72a04db2dadea5f7c71
Morgabor <Jarandall103@gmail.com>
2025-06-12 00:22:45 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+  let pronounArray = ['the', 'our'];
+  let adj = ['Great', 'Big', 'Long', 'Bad', 'Evil'];
+  let noun = ['Jogger', 'Racoon', 'Man', 'Bus'];
+  let end = ['.com','.us'];
+
+for (let pronoun of pronounArray) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let e of end) {
+        console.log(pronoun + a  + n  + e);
+      }
+    }
+  }
+}
+
