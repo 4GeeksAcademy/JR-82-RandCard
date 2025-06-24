d981fc8cd3143e923ed14f7fd06655e75b5ca654
Morgabor <Jarandall103@gmail.com>
2025-06-24 00:21:08 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -6,22 +6,45 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+   let suits = {
+    spade: "♠",
+    heart: "♥",
+    diamond: "♦",
+    club: "♣"
+  };
+  const suit = randomizeSuit();
+  const number = randomizeNumber();
+
+  document.querySelector('.card').className = `card ${suit}`;
+  document.querySelector('.topSuit').textContent = suits[suit];
+  document.querySelector('.botSuit').textContent = suits[suit];
+  document.querySelector('.number').textContent = number;
 };
 
-  let pronounArray = ['the', 'our'];
-  let adj = ['Great', 'Big', 'Long', 'Bad', 'Evil'];
-  let noun = ['Jogger', 'Racoon', 'Man', 'Bus'];
-  let end = ['.com','.us'];
+window.ranOnClick = function() {
+    let suits = {
+    spade: "♠",
+    heart: "♥",
+    diamond: "♦",
+    club: "♣"
+  };
+  const suit = randomizeSuit();
+  const number = randomizeNumber();
 
-for (let pronoun of pronounArray) {
-  for (let a of adj) {
-    for (let n of noun) {
-      for (let e of end) {
-        console.log(pronoun + a  + n  + e);
-      }
-    }
-  }
-}
+  document.querySelector('.card').className = `card ${suit}`;
+  document.querySelector('.topSuit').textContent = suits[suit];
+  document.querySelector('.botSuit').textContent = suits[suit];
+  document.querySelector('.number').textContent = number;
+};
 
+function randomizeNumber() {
+  let number = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let indexNumber = Math.floor(Math.random() * number.length);
+  return number[indexNumber];
+  };
+
+function randomizeSuit () {
+   let suit = ["spade","heart","diamond","club"];
+   let  indexSuit = Math.floor(Math.random() * suit.length); 
+   return suit[indexSuit];
+};
